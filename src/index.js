ac5619c5a28a295ae9c9920aa0fe5e2ee777bf50
Victor Henrique Monteiro Lima <victorhenriqu3@outlook.com>
2022-03-22 18:02:37 -0400

    Create Encode Route with shortid

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const db = require("./Services/db");
 const port = 3000;
+const shortid = require("shortid");
+
 (async () => {
-  const db = require("./Services/db");
   console.log('Começou!');
   console.log('SELECT * FROM CLIENTES');
   const clientes = await db.selectUrls();
@@ -17,8 +19,14 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to URL Shortest App" });
+  res.status(200).json({ message: "Welcome to URL Shortest App" });
 });
+
+// Encode Route : Route responsible for receiving the url that will be stored.
+app.post('/encode', (req, res) => {
+  const short = shortid.generate();
+  res.status(201).json({ id: short, url: req.body.url });
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
